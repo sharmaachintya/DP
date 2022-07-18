a611aee364331d0b6e7869798da6416acb6acbe8
Achintya Sharma <achintya9998@gmail.com>
2022-07-18 17:12:33 +0530

    2D-DP

--- 2D-DP/MinScoreTriangulation.cpp
+++ 2D-DP/MinScoreTriangulation.cpp
@@ -23,7 +23,22 @@ int solveMem(vector<int> &v, int i, int j, vector<vector<int>> &dp)             
 
 int solveTab(vector<int> &v)                                        // BOTTOM UP APPROACH
 {
+    int n = v.size();
+    vector<vector<int>> dp (n, vector<int> (n, 0));
 
+    for (int i=n-1;i>=0;i--)
+    {
+        for (int j=i+2;j<n;j++)
+        {
+            int ans = INT_MAX;
+            for (int k=i+1;k<j;k++)
+            {
+                ans = min(ans, v[i]*v[j]*v[k] + dp[i][k] + dp[k][j]);
+            }
+            dp[i][j] = ans;
+        }
+    }
+    return dp[0][n-1];
 }
 
 int main()
@@ -32,5 +47,6 @@ int main()
     int n = values.size();
     vector<vector<int>> dp (n, vector<int> (n, -1));
     cout<<solveMem(values, 0, n-1, dp)<<endl;
+    cout<<solveTab(values);
 
 }

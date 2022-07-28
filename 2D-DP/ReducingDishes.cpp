ef40b772556919583365c237588b7b58adcd7b83
Achintya Sharma <achintya9998@gmail.com>
2022-07-29 03:57:04 +0530

    2D-DP

---
+++ 2D-DP/ReducingDishes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int solveMem(vector<int> arr, int index, int time, vector<vector<int>> &dp)               // TOP DOWN APPROACH
+{
+    // Base Case
+    if (index == arr.size())
+        return 0;
+
+    if (dp[index][time] != -1)
+        return dp[index][time];
+    
+    int include = arr[index] * (time + 1) + solveMem(arr, index + 1, time + 1, dp);
+    int exclude = 0 + solveMem(arr, index + 1, time, dp);
+
+    return dp[index][time] = max(include, exclude);
+}
+
+int solveTab(vector<int> arr)                                                            // BOTTOM UP APPROACH
+{
+    int n = arr.size();
+    vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+    
+
+}
+
+int main()
+{
+    vector<int> satisfaction = {-1, -8, 0, 5, -9};    
+    int n = satisfaction.size();
+    vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+    cout<<solveMem(satisfaction, 0, 0, dp)<<endl;
+    //cout<<solveTab(satisfaction);
+}

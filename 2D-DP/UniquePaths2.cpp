23b293afb55cf6f7f0f61906fc5e09136ed0e8b1
Achintya Sharma <achintya9998@gmail.com>
2022-11-14 01:33:00 +0530

    2D - DP

--- 2D-DP/UniquePaths2.cpp
+++ 2D-DP/UniquePaths2.cpp
@@ -19,6 +19,31 @@ int solveMem(int i, int j, int n, int m, vector<vector<int>> &mat, vector<vector
     return dp[i][j];
 }
 
+int solveTab(int i, int j, int n, int m, vector<vector<int>> &mat)
+{
+    vector<vector<int>> dp(n, vector<int> (m, 0));
+    for(int i=0;i<n;i++)
+    {
+        for (int j=0;j<m;j++)
+        {
+            if (mat[i][j] == -1)
+                dp[i][j] = 0;
+            else if (i == 0 && j == 0)
+                dp[i][j] = 1;
+            else
+            {
+                int up = 0, left = 0;
+                if (i > 0)
+                    up = dp[i-1][j];
+                if (j > 0)
+                    left = dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+    }
+    return dp[n-1][m-1];
+}
+
 int main()
 {
     int n = 3, m = 3;
@@ -26,5 +51,6 @@ int main()
                                {0, -1, 0},
                                {0, 0, 0}};
     vector<vector<int>> dp (n, vector<int> (m, -1));
-    cout<<solveMem(n-1, m-1, n, m, mat, dp);    
+    cout<<solveMem(n-1, m-1, n, m, mat, dp)<<endl;
+    cout<<solveTab(0, 0, n, m, mat);    
 }

ed5ed0118bfeb051e55f7e73f4b80f10f11dc75d
Achintya Sharma <achintya9998@gmail.com>
2022-11-17 01:47:37 +0530

    2D - DP

--- 2D-DP/MinimumPathSum.cpp
+++ 2D-DP/MinimumPathSum.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int solveMem(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
+
+int solveMem(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)    // Top Down Approach (Memoization)
 {
     // Base case
     if (i == 0 && j == 0)
@@ -16,7 +17,7 @@ int solveMem(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
     return dp[i][j];
 }
 
-int solveTab(int n, int m, vector<vector<int>> &grid)
+int solveTab(int n, int m, vector<vector<int>> &grid)                            // Bottom Up Approach (Tabulation)
 {
     vector<vector<int>> dp (n, vector<int> (m, 0));
     for (int i=0;i<n;i++)

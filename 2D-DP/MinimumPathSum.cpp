18f6938a7c6ba516a914de52ac02d1e2c80e1184
Achintya Sharma <achintya9998@gmail.com>
2022-11-17 01:46:55 +0530

    2D - DP

--- 2D-DP/MinimumPathSum.cpp
+++ 2D-DP/MinimumPathSum.cpp
@@ -16,9 +16,33 @@ int solveMem(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
     return dp[i][j];
 }
 
-int solveTab(vector<vector<int>> &grid)
+int solveTab(int n, int m, vector<vector<int>> &grid)
 {
+    vector<vector<int>> dp (n, vector<int> (m, 0));
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<m;j++)
+        {
+            if (i == 0 && j == 0)
+                dp[i][j] = grid[i][j];
+            else
+            {
+                int up = grid[i][j];
+                if (i > 0)
+                    up = up + dp[i-1][j];
+                else
+                    up = up + 1e9;
+                int left = grid[i][j];
+                if (j > 0)
+                    left = left + dp[i][j-1];
+                else
+                    left = left + 1e9;
 
+                dp[i][j] = min (up, left);
+            }
+        }
+    }
+    return dp[n-1][m-1]; 
 }
 
 int main()
@@ -29,5 +53,5 @@ int main()
     int m = grid[0].size();
     vector<vector<int>> dp(n, vector<int> (m, -1));
     cout<<solveMem(n-1, m-1, grid, dp)<<endl;    
-    cout<<solveTab(grid);
+    cout<<solveTab(n, m, grid);
 }

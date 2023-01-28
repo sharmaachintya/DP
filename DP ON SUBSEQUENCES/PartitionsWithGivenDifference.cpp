34b43d97d86bb20c6a0d60486cf5fb095dde3ce5
Achintya Sharma <achintya9998@gmail.com>
2023-01-28 23:55:09 +0530

    DP ON SUBSEQUENCES

--- DP ON SUBSEQUENCES/PartitionsWithGivenDifference.cpp
+++ DP ON SUBSEQUENCES/PartitionsWithGivenDifference.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solveMem(int ind, int tar, vector<int> &arr, vector<vector<int>> &dp)
+int solveMem(int ind, int tar, vector<int> &arr, vector<vector<int>> &dp)     // TOP DOWN APPROACH (Memoization)
 {
     //Base Cases
     if (ind == 0)
@@ -42,10 +42,49 @@ int partitionDiff(vector<int> &arr, int d)
     return findWays(arr, (totSum - d) / 2);
 }
 
+int solveTab(vector<int> &arr, int target)                           // BOTTOM UP APPROACH (Tabulation)
+{
+    int n = arr.size();
+    vector<vector<int>> dp(n, vector<int> (target+1, 0));
+    if (arr[0] == 0)
+        dp[0][0] = 2;
+    else
+        dp[0][0] = 1;
+    
+    if (arr[0] != 0 && arr[0] <= target)
+        dp[0][arr[0]] = 1;
+    
+    for (int ind=1;ind<n;ind++)
+    {
+        for (int sum=0;sum<=target;sum++)
+        {
+            int notPick = dp[ind-1][sum];
+            int pick = 0;
+            if (arr[ind] <= sum)
+                pick = dp[ind-1][sum-arr[ind]];
+            
+            dp[ind][sum] = notPick + pick;
+        }
+    }
+    return dp[n-1][target];
+}
+
+int partitionDiffTab(vector<int> &arr, int d)
+{
+    int totSum = 0;
+    for (auto it: arr)    
+        totSum += it;
+    
+    if (totSum - d < 0 || (totSum - d) % 2)
+        return 0;
+    return solveTab(arr, (totSum - d) / 2);
+}
+
 int main()
 {
     vector<int> arr = {1, 1, 1, 1};
     int d = 0;
 
     cout<<partitionDiff(arr, d)<<endl;
+    cout<<partitionDiffTab(arr, d);
 }

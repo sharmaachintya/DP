b062adb1962187ddc0e356029faf44a777d6c795
Achintya Sharma <achintya9998@gmail.com>
2023-02-24 16:25:37 +0530

    DP ON STRINGS

--- DP ON STRINGS/LowestCommonSubsequence.cpp
+++ DP ON STRINGS/LowestCommonSubsequence.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+int solveMem(int ind1, int ind2, string s, string t, vector<vector<int>> &dp)
+{
+    // Base Case
+    if (ind1 < 0 || ind2 < 0)
+        return 0;
+    if (dp[ind1][ind2] != -1)
+        return dp[ind1][ind2];
+    if (s[ind1] == t[ind2])
+        return 1 + solveMem(ind1 - 1,ind2 - 1, s, t, dp);
+    
+    return dp[ind1][ind2] = 0 + max(solveMem(ind1 - 1, ind2, s, t, dp), solveMem(ind1, ind2 - 1, s, t, dp));
+}
+
 int main()
 {
-        a
+    string s = "adebc";
+    string t = "dcadb";
+    int n = s.size();
+    int m = t.size();
+    vector<vector<int>> dp(n, vector<int> (m, -1));
+    cout<<solveMem(n - 1, m - 1, s, t, dp)<<endl;
+    
 }

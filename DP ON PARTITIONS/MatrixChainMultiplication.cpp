c4a443f9248adc0612ad41187c16e25f36934c0d
Achintya Sharma <achintya9998@gmail.com>
2023-03-26 01:21:59 +0530

    DP ON PARTITION

--- DP ON PARTITIONS/MatrixChainMultiplication.cpp
+++ DP ON PARTITIONS/MatrixChainMultiplication.cpp
@@ -19,10 +19,35 @@ int solveMem(int i, int j, vector<int> &arr, vector<vector<int>> &dp)        // 
     return dp[i][j] = mini;
 }
 
+int solveTab(vector<int> &arr)                                             // BOTTOM UP APPROACH (Tabultaion)
+{
+    int N = arr.size();
+    vector<vector<int>> dp(N, vector<int> (N, 0));
+    for (int i=0;i<=N-1;i++)
+        dp[i][i] = 0;
+    
+    for (int i=N-1;i>=1;i--)
+    {
+        for (int j=i+1;j<=N-1;j++)
+        {
+            int mini = 1e9;
+            for (int k=i;k<=j-1;k++)
+            {
+                int steps = arr[i-1] * arr[k] * arr[j] + dp[i][k] + dp[k+1][j];
+                mini = min(mini, steps);
+            }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][N-1];
+}
+
+
 int main()
 {
     vector<int> arr = {10, 15, 20, 25};
     int n = arr.size();
     vector<vector<int>> dp(n, vector<int> (n, -1));
-    cout<<solveMem(1, n-1, arr, dp);
+    cout<<solveMem(1, n-1, arr, dp)<<endl;
+    cout<<solveTab(arr);
 }
